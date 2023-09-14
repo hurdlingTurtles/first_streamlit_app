686bc347ea4e795682289604c8b3fb7280ef1a75
hurdlingTurtles <144173170+hurdlingTurtles@users.noreply.github.com>
2023-09-14 16:21:00 -0400

    Update streamlit_app.py
    Moved the fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,12 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # json 2 dataframe
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
