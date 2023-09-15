0d657dbd4a3c98c33d8309060a862ab16fa01241
hurdlingTurtles <144173170+hurdlingTurtles@users.noreply.github.com>
2023-09-15 15:49:21 -0400

    Update streamlit_app.py
    adding buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,13 +48,17 @@ streamlit.write('The user entered ', fruit_choice)
 # Do not run anything into snowflake from above
 streamlit.stop()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("Fruit load lists contain:")
-streamlit.dataframe(my_data_row)
+streamlit.header("Fruit load lists contains:")
+#snowflake related functions
+get get_fruit_load_list()
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+#add button
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_row = get_fruit_load_list()
+    streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thank you for adding ', add_my_fruit)
